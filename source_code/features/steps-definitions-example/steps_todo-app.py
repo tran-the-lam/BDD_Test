70c6973f7556e0843fa78d0e75584de024654372
tran-the-lam <lamtt2403@gmail.com>
2025-01-01 21:24:21 +0700

    update example definition

---
+++ source_code/features/steps-definitions-example/steps_todo-app.py
@@ -0,0 +1,42 @@
+from behave import given, when, then
+
+@given('I am on the to-do list page')
+def step_impl(context):
+    # TODO: Implement step: I am on the to-do list page
+    raise NotImplementedError(""" Step not implemented: I am on the to-do list page """)
+
+
+@when('I enter "Buy groceries" in the task input')
+def step_impl(context):
+    # TODO: Implement step: I enter "Buy groceries" in the task input
+    raise NotImplementedError(""" Step not implemented: I enter "Buy groceries" in the task input """)
+
+
+@when('I press the "Add" button')
+def step_impl(context):
+    # TODO: Implement step: I press the "Add" button
+    raise NotImplementedError(""" Step not implemented: I press the "Add" button """)
+
+
+@then('I should see "Buy groceries" in the list of tasks')
+def step_impl(context):
+    # TODO: Implement step: I should see "Buy groceries" in the list of tasks
+    raise NotImplementedError(""" Step not implemented: I should see "Buy groceries" in the list of tasks """)
+
+
+@given('I have added "Complete homework" to the task list')
+def step_impl(context):
+    # TODO: Implement step: I have added "Complete homework" to the task list
+    raise NotImplementedError(""" Step not implemented: I have added "Complete homework" to the task list """)
+
+
+@when('I click on the "Delete" link next to "Complete homework"')
+def step_impl(context):
+    # TODO: Implement step: I click on the "Delete" link next to "Complete homework"
+    raise NotImplementedError(""" Step not implemented: I click on the "Delete" link next to "Complete homework" """)
+
+
+@then('"Complete homework" should no longer be in the list of tasks')
+def step_impl(context):
+    # TODO: Implement step: "Complete homework" should no longer be in the list of tasks
+    raise NotImplementedError(""" Step not implemented: "Complete homework" should no longer be in the list of tasks """)
