8c5f3a644f734defdc025b82fc8c7240362bbb65
Tran The Lam <32351438+tran-the-lam@users.noreply.github.com>
2025-01-01 11:48:22 +0700

    Create login_steps_definition.example.py

---
+++ source_code/login_steps_definition.example.py
@@ -0,0 +1,24 @@
+from behave import given, when, then
+
+@given('the login window is open')
+def step_impl(context):
+    # TODO: Implement step: the login window is open
+    raise NotImplementedError(""" Step not implemented: the login window is open """)
+
+
+@when('I enter the username "admin" and password "1234"')
+def step_impl(context):
+    # TODO: Implement step: I enter the username "admin" and password "1234"
+    raise NotImplementedError(""" Step not implemented: I enter the username "admin" and password "1234" """)
+
+
+@when('I click the "Login" button')
+def step_impl(context):
+    # TODO: Implement step: I click the "Login" button
+    raise NotImplementedError(""" Step not implemented: I click the "Login" button """)
+
+
+@then('I should see a success message "Login Successful"')
+def step_impl(context):
+    # TODO: Implement step: I should see a success message "Login Successful"
+    raise NotImplementedError(""" Step not implemented: I should see a success message "Login Successful" """)
